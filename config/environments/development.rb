596c9f926837486e1dde391abeb1818267eaada5
jbracy <watertuman@hotmail.com>
2012-02-24 23:03:19 -0500

    upgrade rails to 3.2.0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,12 @@ Weblog::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   
+  # Raise exception on mass assignment protection for Active Record model
+  config.active_record.mass_assignment_sanitizer = :strict
+  
+  # Log the query plan for queries taking more than this (works with SQLite, MySQL, and PostgreSQL)
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
